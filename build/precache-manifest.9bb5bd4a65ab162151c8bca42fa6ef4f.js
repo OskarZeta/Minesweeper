d4567f4b73029b3ce2b033e1acbbae4994d32723
Li Dian <dw4h@yandex.ru>
2019-01-31 19:12:32 +0300

    Deployment to heroku

---
+++ build/precache-manifest.9bb5bd4a65ab162151c8bca42fa6ef4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4061917650704842a89cb1b13e94ee47",
+    "url": "/static/media/timer.40619176.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5cf4719bcfb68aa0e5f8",
+    "url": "/static/js/main.5cf4719b.chunk.js"
+  },
+  {
+    "revision": "2ba2da17f9a33f9da081",
+    "url": "/static/js/1.2ba2da17.chunk.js"
+  },
+  {
+    "revision": "5cf4719bcfb68aa0e5f8",
+    "url": "/static/css/main.d7c7da19.chunk.css"
+  },
+  {
+    "revision": "ded5210480c45e1c98a979c48db0c755",
+    "url": "/index.html"
+  }
+];
